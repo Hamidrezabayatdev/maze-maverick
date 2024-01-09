01299897d7b923d42770d262609cb0eb7d9fe157
aminfaghih <amin.faghih84@gmail.com>
2024-01-09 11:52:37 -0800

    shuffle-done

--- maze.cpp
+++ maze.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include <algorithm>
+#include <random>
+#include<ctime>
+#include <bits/stdc++.h>
+using namespace std;
 int main(){
-    int n;
-    int edit;
-    int a;
+    cout << "1. Create a new map" << endl;
+    cout << "   - 1.1 Easy" << endl << "   - 1.2 Hard" << endl;
+    string level;
+    cin >> level;
+    srand(time(0));
+    if (level == "Easy")
+    {
+        int x, y;
+        cout << "Enter x & y:" << endl;
+        cin >> x >> y;
+        int map[x][y];
+        int zeroCounter = rand()%4 + 2;
+        vector<string> moves;
+        for (int i = 1; i < x; i++)
+            moves.push_back("Right");
+        for (int i = 1; i < y; i++)
+            moves.push_back("Down");
+        random_shuffle(moves.begin(), moves.end());
+    }
+    
 }
