ddaaf3136153515fe4034dab1443d49ae19a88cb
Hamidreza-Bayat <dev.hamidrezabayat@gmail.com>
2024-01-09 11:33:48 +0330

    create a new map

--- maze.cpp
+++ maze.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<ctime>
+#include <bits/stdc++.h>
+using namespace std;
 int main(){
-    int n;
-    int edit;
-    int a;
+    cout << "1. Create a new map" << endl;
+    cout << "   - 1.1 Easy" << endl << "   - 1.2 Hard" << endl;
+    
 }
