e9880e0e7a797c8fef1ee4e3262e47b924cf39fa
Hamidreza-Bayat <dev.hamidrezabayat@gmail.com>
2024-01-17 17:40:33 +0330

    path created

--- maze.cpp
+++ maze.cpp
@@ -1,12 +1,76 @@
 #include<iostream>
+#include<iomanip>
 #include<fstream>
 #include<vector>
 #include <algorithm>
 #include <random>
 #include<ctime>
-#include <bits/stdc++.h>
-#include <windows.h> 
 using namespace std;
 int main(){
-    
+    srand(time(0));
+    int x, y;
+    cout << "Enter row & column:" << endl;
+    cin >> x >> y;
+    int map[x][y];
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            map[i][j] = 0;
+        }
+    }
+    vector<int> path;
+    // 1 = down
+    for (int i = 1; i < x; i++)
+        path.push_back(1);
+    // 2 = right
+    for (int i = 1; i < y; i++)
+        path.push_back(2);
+    random_shuffle(path.begin(), path.end());
+    int iGo = 0, jGo = 0, pathTravel = 0;
+    int sum = 0;
+    vector<int> pathNumbers;
+    for (int i = 0; i < x+y-2; i++)
+    {
+        pathNumbers.push_back(rand()%7-3);
+        while (pathNumbers[i] == 0)
+            pathNumbers[i] = rand()%7-3;
+        sum += pathNumbers[i];
+    }
+    // sum -= pathNumbers[x+y-3];
+    // for (int i = 0; i < x+y-2; i++)
+    // {
+    //     // cout << path[i] << " ";
+    //     cout << pathNumbers[i] << " ";
+    // }
+    cout << endl;
+    for (int i = 0; i < path.size(); i++)
+    {
+        cout << pathNumbers[i] << " ";
+    }
+    cout << endl;
+    while (pathTravel < x+y-2)
+    {
+        if (path[pathTravel] == 1)
+        {
+            map[iGo][jGo] = pathNumbers[pathTravel];
+            pathTravel ++;
+            iGo ++;
+        }
+        else if (path[pathTravel] == 2)
+        {
+            map[iGo][jGo] = pathNumbers[pathTravel];
+            pathTravel ++;
+            jGo ++;
+        }
+    }
+    map[x-1][y-1] = sum;
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            cout << setw(2) << map[i][j] << "  ";
+        }
+        cout << endl;
+    }
 }
