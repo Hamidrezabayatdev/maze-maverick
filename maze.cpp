16c5f16fe1ec5a7fb997b0fa264300e8f86760fb
aminfaghih <amin.faghih84@gmail.com>
2024-01-23 02:23:21 -0800

    hard | map | moves

--- maze.cpp
+++ maze.cpp
@@ -6,6 +6,4 @@
 using namespace std;
 int main(){
     cout << "1. Create a new map" << endl;
-    cout << "   - 1.1 Easy" << endl << "   - 1.2 Hard" << endl;
-    
-}
+    cout << "   - 1.1 Easy" << endl << "   - 1.2 Hard" << e
