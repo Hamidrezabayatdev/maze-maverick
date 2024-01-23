5e965ce19ffa76beab4188f36cfb4527b9c34661
aminfaghih <amin.faghih84@gmail.com>
2024-01-23 02:24:32 -0800

    Revert "hard | map | moves"
    This reverts commit 16c5f16fe1ec5a7fb997b0fa264300e8f86760fb.

--- maze.cpp
+++ maze.cpp
@@ -6,4 +6,6 @@
 using namespace std;
 int main(){
     cout << "1. Create a new map" << endl;
-    cout << "   - 1.1 Easy" << endl << "   - 1.2 Hard" << e
+    cout << "   - 1.1 Easy" << endl << "   - 1.2 Hard" << endl;
+    
+}
