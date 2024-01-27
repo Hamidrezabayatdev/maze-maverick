6169d095a778602d1f60bf6df508dc0d89805315
aminfaghih <amin.faghih84@gmail.com>
2024-01-27 15:15:31 -0800

    funcs

--- maze.cpp
+++ maze.cpp
@@ -1,31 +1,34 @@
 #include<iostream>
+#include<iomanip>
 #include<fstream>
 #include<vector>
 #include <algorithm>
 #include <random>
 #include<ctime>
-#include <bits/stdc++.h>
+#include <dirent.h>
+#include <sys/types.h>
 using namespace std;
-int main(){
-    cout << "1. Create a new map" << endl;
-    cout << "   - 1.1 Easy" << endl << "   - 1.2 Hard" << endl;
-    string level;
-    cin >> level;
-    srand(time(0));
-    if (level == "Easy")
-    {
-        int x, y;
-        cout << "Enter x & y:" << endl;
-        cin >> x >> y;
-        int map[x][y];
-        int zeroCounter = rand()%4 + 2;
-        vector<string> moves;
-        for (int i = 1; i < x; i++)
-            moves.push_back("Right");
-        for (int i = 1; i < y; i++)
-            moves.push_back("Down");
-        random_shuffle(moves.begin(), moves.end());
-        int test;
-    }
-    
-}
+vector<vector<pair<int, int> > > pathscontainer;
+void printMap (int row, int column, vector<int> xHolder, vector<int> yHolder, int** map);
+bool winningState (int row, int column, vector<int> xHolder, vector<int> yHolder, int** map);
+void pushbacks (vector<int>& xHolder, vector<int>& yHolder, vector<char>& moves, char move, int x, int y);
+void popbacks (vector<int>& xHolder, vector<int>& yHolder, vector<char>& moves);
+bool customSort (int a, int b);
+void addHistory (char dt, string globalUsername, string globalMapname, int time1, bool winOrLose);
+void showHistory (int mode);
+void leaderboard (string username);
+void showLeaderboard (int t);
+void easyMapCreate (int t);
+void playground (int t);
+bool usercheck (string username);
+bool mapcheck (string name);
+bool isIn (vector<pair<int,int> >& mypath , int elementx, int elementy );
+void findPath( int x, int y, int len, vector<pair<int, int> >& path, int ROW, int COL, int** map);
+bool tekrar (int tmp);
+void hardMap (int t);
+struct player
+{
+    string username;
+    int wins;
+    int time;
+};
